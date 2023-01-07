49ac3db562741cb46be4669e204d0f8f0a2b4af4
Зоя <114188282+MAKAROVSKAYA@users.noreply.github.com>
2023-01-07 23:48:34 +0300

    zadanie 4-2v1

---
+++ HWZ4/zadanie4-2v1.py
@@ -0,0 +1,30 @@
+# Задание № Задача 2 Вариант 1 - список
+
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+# 
+# n     54
+# out   [2, 3, 3, 3]
+# in    9990
+# out   [2, 3, 3, 3, 5, 37]
+# in    650
+# out   [2, 5, 5, 13]
+
+def with_multipliers (number):
+    lst = []
+    i = 2                       # первое простое число возможного сомножителя
+    multiplier = number
+    while i <= number:
+        if multiplier % i == 0:
+            lst.append(i)
+            multiplier //= i
+            i = 2
+        else:
+            i += 1
+    return lst
+
+number = int(input("Введите целое число: "))
+lst = []
+lst = with_multipliers (number)
+print(f"Простые множители числа {number} представлены: {lst}")
+
