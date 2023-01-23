8b30120a07ea7c889ecaecd99e399764faf4d1db
Зоя <114188282+MAKAROVSKAYA@users.noreply.github.com>
2023-01-24 00:54:39 +0300

    zadanie6-3

---
+++ HWZ6/zadanie6-3.py
@@ -0,0 +1,13 @@
+# Урок 6 Задание 3
+# Написать функцию, аргументы имена сотрудников, возвращает словарь,
+# ключи — первые буквы имён, значения — списки, содержащие имена,
+# начинающиеся с соответствующей буквы.
+
+from itertools import groupby
+
+def thesaurus(*args):
+    if "" not in args:
+        return {ch: list(names) for ch, names in groupby(sorted(args), key=lambda i: i[0]) if ch}
+    return "Error"
+
+print(thesaurus("Нил", "Марфа", "Арсений", "Любовь", "Денис", "Никодим", "Алексей", "Дмитрий"))
